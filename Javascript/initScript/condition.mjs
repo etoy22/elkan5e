cab072adafb9ff63f300b0bbb671023b65f45404
etoy22 <ethanleider@gmail.com>
2024-01-15 20:10:13 -0500

    Elkan 5e Compendium Folder
    Elkan 5e Content will now come as a Folder

--- Javascript/initScript/condition.mjs
+++ Javascript/initScript/condition.mjs
@@ -6,16 +6,16 @@ export function conditions(){
 
     //Add conditions
     CONFIG.DND5E.conditionTypes.confused = "Confused";
+    CONFIG.DND5E.conditionTypes.dazed = "Dazed";
     CONFIG.DND5E.conditionTypes.dominated = "Dominated";
     CONFIG.DND5E.conditionTypes.drained = "Drained";
     CONFIG.DND5E.conditionTypes.goaded = "Goaded";
     CONFIG.DND5E.conditionTypes.hasted = "Hasted";
+    CONFIG.DND5E.conditionTypes.muted = "Muted";
     CONFIG.DND5E.conditionTypes.siphoned = "Siphoned";
     CONFIG.DND5E.conditionTypes.slowed = "Slowed";
     CONFIG.DND5E.conditionTypes.surprised = "Surprised";
     CONFIG.DND5E.conditionTypes.weakened = "Weakened";
-    CONFIG.DND5E.conditionTypes.muted = "Muted";
-    CONFIG.DND5E.conditionTypes.dazed = "Dazed";
 }
 
 
--- Javascript/initScript/weapon.mjs
+++ Javascript/initScript/weapon.mjs
@@ -8,15 +8,17 @@ export function weaponTotal(){
 }
 
 
-
 /**
  * Changes the weapon properties in Foundry to match Elkan 5e.
  */
 export function weaponProperties(){
+    //Adding new weapon Properties
     CONFIG.DND5E.weaponProperties.coldIron = "Cold Iron"
     CONFIG.DND5E.physicalWeaponProperties.coldIron = "Cold Iron"
     CONFIG.DND5E.weaponProperties.mou = "Mounted"
     CONFIG.DND5E.weaponProperties.unw = "Unwieldy"
+
+    //Removing Weapon Property
     delete CONFIG.DND5E.weaponProperties.spc
 }
 
@@ -25,7 +27,6 @@ export function weaponProperties(){
  * Removes weapons that aren't in Elkan 5e in Foundry.
  */
 export function weaponTypes(){
-    //Removed Weapons
     delete CONFIG.DND5E.weaponIds.flail
     delete CONFIG.DND5E.weaponIds.net
     delete CONFIG.DND5E.weaponIds.trident
