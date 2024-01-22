67079d83539d01090d319f3faaf800d1ba68cfe6
etoy22 <ethanleider@gmail.com>
2024-01-22 01:59:11 -0500

    Added possible updates
    Added Possible TODOs and added one more thing to the Changelog

--- module/classes/cleric.mjs
+++ module/classes/cleric.mjs
@@ -27,3 +27,27 @@ export function advance_cleric(toCreate,toUpdate){
         }
     }
 }
+
+
+/**
+ * TODO: This should see if a creature has how much health remaining and then tell the player the 
+ * amount that they have left for the Healing overflow ability. Maybe use dnd5e.rollDamage
+ * 
+ * Adds functionality to Healing Overflow
+ *   @param {object[]} item - The Actor that rolled initiative.
+ *   @param {string} roll - The resulting roll.
+ */
+export function healOver(actor,combatants){    
+    console.log("Nothing ")
+}
+
+/**
+ * TODO: This would apply healing when the caster makes a healing spell
+ * 
+ * Adds functionality to Infused Healer
+ *   @param 
+ *   @param 
+ */
+export function infuseHeal(actor,combatants){    
+    console.log("Nothing ")
+}
