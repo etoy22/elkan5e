5a90394235443df14a5758c854103725433955f4
etoy22 <ethanleider@gmail.com>
2024-01-22 01:33:50 -0500

    Resources Spending
    Class resources will now be spent when being added via advancement

--- module/elkan5e.mjs
+++ module/elkan5e.mjs
@@ -3,7 +3,11 @@ import {conditions} from "./setup/condition.mjs"
 import {weaponTotal} from "./setup/weapon.mjs"
 import {armor} from "./setup/armor.mjs"
 import {language} from "./setup/language.mjs"
-import {focus} from "./attacking/focus_slaugter.mjs"
+import {focus} from "./classes/barbarian.mjs"
+import {advance_monk} from "./classes/monk.mjs"
+import {advance_sorcerer} from "./classes/sorcerer.mjs"
+import {advance_cleric} from "./classes/cleric.mjs"
+
 
 Hooks.once("init", () => {
     console.log("Elkan 5e  |  Initializing Elkan 5e")
@@ -14,8 +18,18 @@ Hooks.once("init", () => {
     language();
 });
 
-    
-Hooks.on(`dnd5e.preRollAttack`, (item, config) => {
+/**
+ * Things that occur when an attack is declaired
+ */
+Hooks.on("dnd5e.preRollAttack", (item, config) => {
     focus(item, config)
-}) 
-    
+});
+
+/**
+ * On advancement type abilities
+ */
+Hooks.on("dnd5e.preAdvancementManagerComplete", (advancementManager,actorUpdates,toCreate,toUpdate,toDelete) => {
+    advance_monk(toCreate,toUpdate)
+    advance_sorcerer(toCreate,toUpdate)
+    advance_cleric(toCreate,toUpdate)
+});
