665aef16ee6c00a87f5f922a20e3a0486a008785
etoy22 <ethanleider@gmail.com>
2024-01-25 11:31:42 -0500

    Delete target.mjs
    Changed our decision on the it existing

--- module/scripts/rules/target.mjs
+++
@@ -1,9 +0,0 @@
-/*
- * Adding a target option called Visible Creature
- */
-export function target (){
-    CONFIG.DND5E.targetTypes.visibleTarget = "Visible Creature"
-    CONFIG.DND5E.individualTargetTypes.visibleTarget = "Visible Creature"
-    console.log("Elkan 5e targetTypes |", CONFIG.DND5E.targetTypes)
-    console.log("Elkan 5e targetTypes |", CONFIG.DND5E.individualTargetTypes)
-}
