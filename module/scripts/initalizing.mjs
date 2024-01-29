bc50f2b1eaf7788fc967271d1b01c48cb225eaab
etoy22 <ethanleider@gmail.com>
2024-01-29 12:25:33 -0500

    Spell Component Loot Subtype
    Added Spell Components as a subtype of Loot Types

--- module/scripts/initalizing.mjs
+++ module/scripts/initalizing.mjs
@@ -2,6 +2,7 @@ import { armor } from "./rules/armor.mjs";
 import { conditions } from "./rules/condition.mjs";
 import { subFeatures } from "./rules/feats.mjs";
 import { language } from "./rules/language.mjs";
+import { mats } from "./rules/material.mjs";
 import { speed } from "./rules/speed.mjs";
 import { tools } from "./rules/tools.mjs";
 import { weapons } from "./rules/weapon.mjs";
@@ -16,6 +17,7 @@ export function init(){
     language();
     speed()
     subFeatures()
+    mats()
     // TODO: the following below
     // target() 
 } 
