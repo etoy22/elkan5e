7eebb60d3e249f6df36528e95ab69a90eb0ce3c4
etoy22 <ethanleider@gmail.com>
2024-02-04 21:59:09 -0500

    Push of some of the updates

---
+++ module/scripts/rules/skill.mjs
@@ -0,0 +1,44 @@
+export function skills(){
+    console.log("Elkan 5e  |  Initializing Skills")
+
+    //Reference on character sheet
+    CONFIG.DND5E.skills.acr.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.nJvKshCeUsYho87K"
+    CONFIG.DND5E.skills.ani.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.JR9h0nL97GegQ9Vz"
+    CONFIG.DND5E.skills.arc.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.Cc49eyAgMrF1GIjH"
+    CONFIG.DND5E.skills.ath.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.1lTpjCIaINKzvmKI"
+    CONFIG.DND5E.skills.dec.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.UDcxonEumLH5vEQu"
+    CONFIG.DND5E.skills.his.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.o9V0Z91HWH84JHda"
+    CONFIG.DND5E.skills.ins.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.HAKuuUMWW3pRCMoL"
+    CONFIG.DND5E.skills.inv.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.1VOLgBW7kkGwaPbH"
+    CONFIG.DND5E.skills.itm.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.3WqeQryCXL1gtaEo"
+    CONFIG.DND5E.skills.med.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.bEEOxmai3Q08nTfT"
+    CONFIG.DND5E.skills.nat.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.0v0AbmZaL3N0zeO2"
+    CONFIG.DND5E.skills.per.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.X3dKHNVduLjYxR1x"
+    CONFIG.DND5E.skills.prc.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.pIFI2y2qLS9ovm0C"
+    CONFIG.DND5E.skills.prf.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.9bVttJ5qNpwiOpzL"
+    CONFIG.DND5E.skills.rel.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.KGv0Bkb9thO9K4xJ"
+    CONFIG.DND5E.skills.slt.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.ynZa3sl1E681sRlP"
+    CONFIG.DND5E.skills.ste.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.LnS51AK4Yi0P53QT"
+    CONFIG.DND5E.skills.sur.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.6KIdxNMhOuvZUMxc"
+    
+    //Reference to these using &Reference
+    CONFIG.DND5E.enrichmentLookup.skills.arc.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.nJvKshCeUsYho87K"
+    CONFIG.DND5E.enrichmentLookup.skills.ani.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.JR9h0nL97GegQ9Vz"
+    CONFIG.DND5E.enrichmentLookup.skills.arc.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.Cc49eyAgMrF1GIjH"
+    CONFIG.DND5E.enrichmentLookup.skills.ath.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.1lTpjCIaINKzvmKI"
+    CONFIG.DND5E.enrichmentLookup.skills.dec.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.UDcxonEumLH5vEQu"
+    CONFIG.DND5E.enrichmentLookup.skills.his.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.o9V0Z91HWH84JHda"
+    CONFIG.DND5E.enrichmentLookup.skills.ins.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.HAKuuUMWW3pRCMoL"
+    CONFIG.DND5E.enrichmentLookup.skills.inv.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.1VOLgBW7kkGwaPbH"
+    CONFIG.DND5E.enrichmentLookup.skills.itm.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.3WqeQryCXL1gtaEo"
+    CONFIG.DND5E.enrichmentLookup.skills.med.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.bEEOxmai3Q08nTfT"
+    CONFIG.DND5E.enrichmentLookup.skills.nat.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.0v0AbmZaL3N0zeO2"
+    CONFIG.DND5E.enrichmentLookup.skills.per.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.X3dKHNVduLjYxR1x"
+    CONFIG.DND5E.enrichmentLookup.skills.prc.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.pIFI2y2qLS9ovm0C"
+    CONFIG.DND5E.enrichmentLookup.skills.prf.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.9bVttJ5qNpwiOpzL"
+    CONFIG.DND5E.enrichmentLookup.skills.rel.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.KGv0Bkb9thO9K4xJ"
+    CONFIG.DND5E.enrichmentLookup.skills.slt.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.ynZa3sl1E681sRlP"
+    CONFIG.DND5E.enrichmentLookup.skills.ste.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.LnS51AK4Yi0P53QT"
+    CONFIG.DND5E.enrichmentLookup.skills.sur.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.rv19GFzEa0nMTuAF.JournalEntryPage.6KIdxNMhOuvZUMxc"
+
+}
