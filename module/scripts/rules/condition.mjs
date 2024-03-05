0e53850a1d83eb1da54529db645ec70f86c06ec7
etoy22 <ethanleider@gmail.com>
2024-03-05 01:04:02 -0500

    Lots of stuffs
    Saving after a huge blunder. All the backend stuff is broken for some reason

--- module/scripts/rules/condition.mjs
+++ module/scripts/rules/condition.mjs
@@ -4,16 +4,16 @@
 export function conditions(){
     console.log("Elkan 5e  |  Initializing Conditions")
     //Replace icons
-    // CONFIG.DND5E.conditionTypes.blinded.icon = "modules/elkan5e/icons/blinded.svg"
-    // CONFIG.DND5E.conditionTypes.deafened.icon = "modules/elkan5e/icons/deafened.svg"
-    // CONFIG.DND5E.conditionTypes.frightened.icon = "modules/elkan5e/icons/frightened.svg"
-    // CONFIG.DND5E.conditionTypes.grappled.icon = "modules/elkan5e/icons/grappled.svg"
-    // CONFIG.DND5E.conditionTypes.invisible.icon = "modules/elkan5e/icons/invisible.svg"
-    // CONFIG.DND5E.conditionTypes.paralyzed.icon = "modules/elkan5e/icons/paralyzed.svg"
-    // CONFIG.DND5E.conditionTypes.petrified.icon = "modules/elkan5e/icons/petrified.svg"
-    // CONFIG.DND5E.conditionTypes.poisoned.icon = "modules/elkan5e/icons/poisoned.svg"
-    // CONFIG.DND5E.conditionTypes.prone.icon = "modules/elkan5e/icons/prone.svg"
-    // CONFIG.DND5E.conditionTypes.unconscious.icon = "icons/svg/unconscious.svg"
+    CONFIG.DND5E.conditionTypes.blinded.icon = "modules/elkan5e/icons/blinded.svg"
+    CONFIG.DND5E.conditionTypes.deafened.icon = "modules/elkan5e/icons/deafened.svg"
+    CONFIG.DND5E.conditionTypes.frightened.icon = "modules/elkan5e/icons/frightened.svg"
+    CONFIG.DND5E.conditionTypes.grappled.icon = "modules/elkan5e/icons/grappled.svg"
+    CONFIG.DND5E.conditionTypes.invisible.icon = "modules/elkan5e/icons/invisible.svg"
+    CONFIG.DND5E.conditionTypes.paralyzed.icon = "modules/elkan5e/icons/paralyzed.svg"
+    CONFIG.DND5E.conditionTypes.petrified.icon = "modules/elkan5e/icons/petrified.svg"
+    CONFIG.DND5E.conditionTypes.poisoned.icon = "modules/elkan5e/icons/poisoned.svg"
+    CONFIG.DND5E.conditionTypes.prone.icon = "modules/elkan5e/icons/prone.svg"
+    CONFIG.DND5E.conditionTypes.unconscious.icon = "icons/svg/unconscious.svg"
     
     //Replace References
     CONFIG.DND5E.conditionTypes.blinded.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.eS0uzU55fprQJqIt.JournalEntryPage.SXTqmewRrCwPS8yW"
@@ -32,13 +32,25 @@ export function conditions(){
     CONFIG.DND5E.conditionTypes.unconscious.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.eS0uzU55fprQJqIt.JournalEntryPage.ZwhWWUPJvpFCz8sK"
     CONFIG.DND5E.conditionTypes.exhaustion.reference ="Compendium.elkan5e.elkan5e-rules.JournalEntry.eS0uzU55fprQJqIt.JournalEntryPage.mPzXN6MW8L6ePFmq"
     
+
+    //Concentration
+    CONFIG.DND5E.spellTags.concentration.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.eS0uzU55fprQJqIt.JournalEntryPage.4ZOHN6tGvj54J6Kv"
+
     //TODO: Talk about these condition   
     CONFIG.DND5E.rules.concentrating = "Compendium.elkan5e.elkan5e-rules.JournalEntry.eS0uzU55fprQJqIt.JournalEntryPage.zY2CHn81NAqSs6gh"
     CONFIG.DND5E.spellTags.concentration.reference = "Compendium.elkan5e.elkan5e-rules.JournalEntry.eS0uzU55fprQJqIt.JournalEntryPage.4ZOHN6tGvj54J6Kv"
     CONFIG.DND5E.rules.surprise = "Compendium.elkan5e.elkan5e-rules.JournalEntry.eS0uzU55fprQJqIt.JournalEntryPage.QOZeW0m8RCdVg6UE"
-
-    //Add conditions
     
+    
+    //Cover
+    CONFIG.DND5E.rules.cover = "Compendium.elkan5e.elkan5e-rules.JournalEntry.eS0uzU55fprQJqIt.JournalEntryPage.d2hBqe6EYHX2mxKD"
+    CONFIG.DND5E.rules.halfcover = "Compendium.elkan5e.elkan5e-rules.JournalEntry.eS0uzU55fprQJqIt.JournalEntryPage.1BmTbnT3xDPqv9dq"
+    CONFIG.DND5E.rules.threequarterscover = "Compendium.elkan5e.elkan5e-rules.JournalEntry.eS0uzU55fprQJqIt.JournalEntryPage.82ph4sMqvhxjLbiw"
+    CONFIG.DND5E.rules.totalcover = "Compendium.elkan5e.elkan5e-rules.JournalEntry.eS0uzU55fprQJqIt.JournalEntryPage.hY5s70xMeG5ISFUA"
+    
+
+    //For now this is commented out while we work on effecting icons
+    //Add conditions
     CONFIG.DND5E.conditionTypes.confused = {
         label:"Confused",
         reference:"Compendium.elkan5e.elkan5e-rules.JournalEntry.eS0uzU55fprQJqIt.JournalEntryPage.WJFtNc5UraHVrV5V"
@@ -81,19 +93,12 @@ export function conditions(){
         label: "Weakened",
         reference:"Compendium.elkan5e.elkan5e-rules.JournalEntry.eS0uzU55fprQJqIt.JournalEntryPage.iJT3cWvyTNBv1L5h"
     };
-    
-    //Cover
-    CONFIG.DND5E.rules.cover = "Compendium.elkan5e.elkan5e-rules.JournalEntry.eS0uzU55fprQJqIt.JournalEntryPage.d2hBqe6EYHX2mxKD"
-    CONFIG.DND5E.rules.halfcover = "Compendium.elkan5e.elkan5e-rules.JournalEntry.eS0uzU55fprQJqIt.JournalEntryPage.1BmTbnT3xDPqv9dq"
-    CONFIG.DND5E.rules.threequarterscover = "Compendium.elkan5e.elkan5e-rules.JournalEntry.eS0uzU55fprQJqIt.JournalEntryPage.82ph4sMqvhxjLbiw"
-    CONFIG.DND5E.rules.totalcover = "Compendium.elkan5e.elkan5e-rules.JournalEntry.eS0uzU55fprQJqIt.JournalEntryPage.hY5s70xMeG5ISFUA"
-    
+    //Delete
     delete CONFIG.DND5E.statusEffects.bleeding
     delete CONFIG.DND5E.statusEffects.burrowing
     delete CONFIG.DND5E.statusEffects.curse
     delete CONFIG.DND5E.statusEffects.flying
     delete CONFIG.DND5E.statusEffects.marked
-    //Delete
 }
 
 
