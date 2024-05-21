057d8635aab90fd89978a277d4d65748700c585d
etoy22 <ethanleider@gmail.com>
2024-05-21 03:11:40 -0400

    Changelog + Referencing + Automation

--- module/scripts/initalizing.mjs
+++ module/scripts/initalizing.mjs
@@ -2,10 +2,10 @@ import { armor } from "./rules/armor.mjs";
 import { conditions, icons } from "./rules/condition.mjs";
 import { language } from "./rules/language.mjs";
 import { activation, ruleType, mats, subFeatures } from "./format.mjs";
-import { skills } from "./rules/skill.mjs";
 import { speed } from "./rules/speed.mjs";
 import { tools } from "./rules/tools.mjs";
 import { weapons } from "./rules/weapon.mjs";
+import { combat, skills } from "./rules/references.mjs";
 
 
 
@@ -21,6 +21,7 @@ export function init(){
     ruleType()
     activation()
     icons()
+    combat()
     // TODO: the following below
     // speed()
 } 
