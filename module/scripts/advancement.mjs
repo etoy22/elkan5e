d09e30317fbd3c0b9ac36851453601c75642faa7
etoy22 <ethanleider@gmail.com>
2024-05-24 20:12:40 -0400

    Revert "Pushing for backup"
    This reverts commit 1011f92dcfee4b7551d668f852b7c960a654e1d0.

---
+++ module/scripts/advancement.mjs
@@ -0,0 +1,20 @@
+import {advance_monk} from "./classes/monk.mjs"
+import {advance_sorcerer} from "./classes/sorcerer.mjs"
+import {advance_cleric} from "./classes/cleric.mjs"
+import {advance_bard} from "./classes/bard.mjs"
+import { advance_paladin } from "./classes/paladin.mjs"
+import { advance_ranger } from "./classes/ranger.mjs"
+
+/**
+ * On advancement type abilities
+ */
+export function advance(toCreate,toUpdate){
+    advance_bard(toCreate,toUpdate)
+    advance_cleric(toCreate,toUpdate)
+    advance_monk(toCreate,toUpdate)
+    advance_paladin(toCreate,toUpdate)
+    advance_sorcerer(toCreate,toUpdate)
+    advance_ranger(toCreate,toUpdate)
+}
+
+
