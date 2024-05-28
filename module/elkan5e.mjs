47466710af24db2f387c526390fc3c34b4148df1
etoy22 <ethanleider@gmail.com>
2024-05-28 18:58:13 -0400

    Squashed commit of the following:
    commit 5facd3ef3f37b0a9268cd9b602ee95cd9701b99e
    Author: etoy22 <ethanleider@gmail.com>
    Date:   Fri May 24 20:12:40 2024 -0400
    
        Revert "Pushing for backup"
    
        This reverts commit 0231c343dfac2b4fcef7ba31fd35091d647caf75.

--- module/elkan5e.mjs
+++ module/elkan5e.mjs
@@ -1,4 +1,5 @@
 import {focus} from "./scripts/classes/barbarian.mjs"
+import { advance } from "./scripts/advancement.mjs";
 import { init} from "./scripts/initalizing.mjs";
 
 Hooks.once("init", () => {
@@ -14,6 +15,10 @@ Hooks.on("dnd5e.preRollAttack", (item, config) => {
     focus(item,config)
 });
 
+
+Hooks.on("dnd5e.preAdvancementManagerComplete", (advancementManager,actorUpdates,toCreate,toUpdate,toDelete) => {
+    advance(toCreate,toUpdate)
+});
 
 // TODO: This works with getting midi saves
 // Hooks.on("dnd5e.preRollAbilitySave", (actor,roll,abilityID) => {
