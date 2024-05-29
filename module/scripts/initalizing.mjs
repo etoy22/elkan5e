5ef4574530f272b8803396a53b09f2672ac1820a
etoy22 <ethanleider@gmail.com>
2024-05-28 20:16:42 -0400

    Fixed Bug

--- module/scripts/initalizing.mjs
+++ module/scripts/initalizing.mjs
@@ -1,27 +1,23 @@
 import { armor } from "./rules/armor.mjs";
 import { conditions, icons } from "./rules/condition.mjs";
 import { language } from "./rules/language.mjs";
-import { activation, ruleType, mats, subFeatures } from "./rules/format.mjs";
-import { speed } from "./rules/speed.mjs";
+import { formating } from "./rules/format.mjs";
 import { tools } from "./rules/tools.mjs";
 import { weapons } from "./rules/weapon.mjs";
-import { combat, skills } from "./rules/references.mjs";
+import { references } from "./rules/references.mjs";
 
 
 
 export function init(){
+    console.log("Elkan 5e | Init")
     tools();
     conditions();
     weapons();
     armor();
     language();
-    subFeatures()
-    mats()
-    skills()
-    ruleType()
-    activation()
     icons()
-    combat()
+    formating()
+    references()
     // TODO: the following below
     // speed()
 } 
