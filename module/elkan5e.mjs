55f439594da59f92e7cc7598f09d75a4ecc5f967
etoy22 <ethanleider@gmail.com>
2024-06-08 18:33:41 -0400

    Update elkan5e.mjs

--- module/elkan5e.mjs
+++ module/elkan5e.mjs
@@ -8,11 +8,26 @@ Hooks.once("init", () => {
 
 /**
  * Things that occur when an attack is declaired
- */
-Hooks.on("dnd5e.preRollAttack", (item, config) => {
-    focus(item,config)
+*/
+Hooks.on("dnd5e.preCreateItemTemplate", (item, templateData) => {
+    console.log("Elkan 5e  |  item", item)
+    console.log("Elkan 5e  |  config", templateData)
+    
+    if (item.name == "Web"){
+        console.log("Elkan 5e  |  Hope")
+        item.system.target.value = ""+20 + 5*(item.system.level - 2)
+        templateData.width = 20 + 5*(item.system.level - 2)
+    }
 });
 
+
+
+// TODO: This works with getting midi saves
+// Hooks.on("dnd5e.preRollAbilitySave", (actor,roll,abilityID) => {
+//     console.log("Elkan 5e Actor", actor)
+//     console.log("Elkan 5e Roll", roll)
+//     console.log("Elkan 5e abilityID", abilityID)
+// });
 
 
 // TODO: This works with getting midi saves
