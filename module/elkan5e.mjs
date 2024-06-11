7f246e5f726222733540a600e3d33b222fd4ac9b
etoy22 <ethanleider@gmail.com>
2024-06-11 16:30:52 -0400

    Create Dialog

--- module/elkan5e.mjs
+++ module/elkan5e.mjs
@@ -3,6 +3,7 @@ import { init} from "./scripts/initalizing.mjs";
 Hooks.once("init", () => {
     console.log("Elkan 5e  |  Initializing Elkan 5e")
     init()
+    d.render(true);
 });
 
 
@@ -13,7 +14,22 @@ Hooks.on("dnd5e.preRollAttack", (item, config) => {
     focus(item,config)
 });
 
-
+let d = new Dialog({
+    title: "Test Dialog",
+    content: "<p>You must choose either Option 1, or Option 2</p>",
+    buttons: {
+     one: {
+      icon: '<i class="fas fa-check"></i>',
+      label: "Option One",
+      callback: () => console.log("Chose One")
+     },
+     two: {
+      icon: '<i class="fas fa-times"></i>',
+      label: "Option Two",
+      callback: () => console.log("Chose Two")
+     }
+    }
+});
 
 // TODO: This works with getting midi saves
 // Hooks.on("dnd5e.preRollAbilitySave", (actor,roll,abilityID) => {
