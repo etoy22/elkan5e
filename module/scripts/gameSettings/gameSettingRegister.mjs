2b229a1312ab4c6ab84daf397bf73c089e232fb9
etoy22 <ethanleider@gmail.com>
2024-07-02 16:12:11 -0400

    Updated Dialog Box
    Dialog Box will stop showing if the player puts Do Not Show Again

---
+++ module/scripts/gameSettings/gameSettingRegister.mjs
@@ -0,0 +1,10 @@
+export function gameSettingRegister(){
+    game.settings.register("elkan5e", "dialogShown", {
+        name: "Dialog Shown",
+        hint: "Indicates whether the dialog has been shown for this user",
+        scope: "client",
+        config: false,
+        type: Boolean,
+        default: false,
+      });
+}
