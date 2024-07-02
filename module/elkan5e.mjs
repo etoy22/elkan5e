2647e26549699d68bc3943be79389afddcf36444
etoy22 <ethanleider@gmail.com>
2024-07-02 16:12:11 -0400

    Updated Dialog Box
    Dialog Box will stop showing if the player puts Do Not Show Again

--- module/elkan5e.mjs
+++ module/elkan5e.mjs
@@ -1,11 +1,19 @@
+import { gameSettingRegister } from "./scripts/gameSettings/gameSettingRegister.mjs";
+import { startDialog } from "./scripts/gameSettings/startDialog.mjs";
 import { init} from "./scripts/initalizing.mjs";
+
+
+
 
 Hooks.once("init", () => {
     console.log("Elkan 5e  |  Initializing Elkan 5e")
     init()
-    d.render(true);
+    gameSettingRegister()
 });
 
+Hooks.once('ready', async () => {
+    startDialog()
+});
 
 /**
  * Things that occur when an attack is declaired
@@ -14,22 +22,6 @@ Hooks.on("dnd5e.preRollAttack", (item, config) => {
     focus(item,config)
 });
 
-let d = new Dialog({
-    title: "Test Dialog",
-    content: "<p>You must choose either Option 1, or Option 2</p>",
-    buttons: {
-     one: {
-      icon: '<i class="fas fa-check"></i>',
-      label: "Option One",
-      callback: () => console.log("Chose One")
-     },
-     two: {
-      icon: '<i class="fas fa-times"></i>',
-      label: "Option Two",
-      callback: () => console.log("Chose Two")
-     }
-    }
-});
 
 // TODO: This works with getting midi saves
 // Hooks.on("dnd5e.preRollAbilitySave", (actor,roll,abilityID) => {
