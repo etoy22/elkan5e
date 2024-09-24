9d49deec7286a2549b46c7a5d6580064872d9424
42547082+deepsource-io[bot]@users.noreply.github.com=EC2 Default User <ec2-user@ip-172-31-34-12.us-east-2.compute.internal>
2024-09-24 04:02:46 +0000

    Back Up after help

--- module/scripts/initalizing.mjs
+++ module/scripts/initalizing.mjs
@@ -5,6 +5,7 @@ import { formating } from "./rules/format.mjs";
 import { tools } from "./rules/tools.mjs";
 import { weapons } from "./rules/weapon.mjs";
 import { references } from "./rules/references.mjs";
+import { scroll } from "./rules/scroll.mjs";
 
 
 
@@ -15,8 +16,9 @@ export function init(){
     weapons();
     armor();
     language();
-    icons()
-    formating()
+    icons();
+    formating();
+	scroll();
     console.log("Elkan 5e  |  Done Initalizing")
     // TODO: the following below
     // speed()
