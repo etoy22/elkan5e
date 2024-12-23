34f1cdc4f7ecd9bb1e6a4d57a7bb8833074298fb
etoy22 <ethanleider@gmail.com>
2024-12-23 13:10:57 -0500

    Wild Surge Has been implemented as well as comments
    Made it so that all possibilities of an individual wild surging has been accounted for. This is the temporary measure until switching to  flags.midi-qol.onUseMacroName

--- src/elkan5e.mjs
+++ src/elkan5e.mjs
@@ -6,6 +6,7 @@ import { perLeader } from "./module/classes/fighter.mjs";
 import { healOver, infuseHeal } from "./module/classes/cleric.mjs";
 import { archDruid } from "./module/classes/druid.mjs";
 import { feral, improvedFeral } from "./module/classes/barbarian.mjs";
+import { wildSurge } from "./module/classes/sorcerer.mjs";
 
 Hooks.once("init", () => {
     console.log("Elkan 5e  |  Initializing Elkan 5e");
@@ -19,102 +20,48 @@ Hooks.once('ready', async () => {
 });
 
 /**
- * Things that occur when an attack is declared
- *   @param {object} item - The item used.
- *   @param {object} config - The configuration for the attack roll.
+ * Handle focus when an attack is declared.
+ * @param {object} item - The item used.
+ * @param {object} config - The configuration for the attack roll.
  */
 Hooks.on("dnd5e.preRollAttackV2", (item, config) => {
-    // Handle focus when an attack is declared
     focus(item, config);
 });
 
-/*
-    Automation for Undead Nature
- *   @param {object} config - The configuration for the hit die roll.
+/**
+ * Adjust hit die roll for Undead Nature feature.
+ * @param {object} config - The configuration for the hit die roll.
  */
 Hooks.on("dnd5e.preRollHitDieV2", (config) => {
     console.log("preRollHitDieV2 Hook Triggered", { config });
-    // Adjust hit die roll for Undead Nature feature
     const actor = config.subject;
     if (actor.items.find(feature => feature.name === "Undead Nature") && !actor.effects.find(effect => effect.name === "Gentle Repose")) {
         config.rolls[0].parts[0] += '-@abilities.con.mod';
     }
 });
 
-// TODO: Move all of these to macros use this -> flags.midi-qol.onUseMacroName | Custom | ItemMacro, preDamageApplication
-
-
 /**
- * Handle pre-use activity for Wild Mage Sorcerer
- *   @param {object} activity - The activity performed.
- *   @param {object} usageConfig - The usage configuration.
+ * Handle post-use activity for Wild Mage Sorcerer.
+ * @param {object} activity - The activity performed.
  */
 Hooks.on("dnd5e.postUseActivity", async (activity) => {
-    // console.log(activity)
-    // Wild Magic Surge
-    const actor = activity.actor;
-    const item = activity.item;
-    if (item.type === "spell" && item.system.level > 0 && actor.items.find(i => i.name === "Random Wild Surge")) {
-        const roll = await new Roll("1d6").roll({ async: true });
-        await roll.toMessage({
-            flavor: "Random Wild Surge",
-            speaker: ChatMessage.getSpeaker({ actor: actor })
-        });
-        if (roll.total >= 5) {
-            const tableUUIDs = [
-                null, // No table for level 0
-                "GDE5tgmRfX1GiOQs",
-                "mxwqgbo7xnNXSnIm",
-                "Fwl1JxM19LzeYxjJ",
-                "0TDp89O9iGt4zovG",
-                "V4BRRp6vWntQNVwa",
-                "4TsMG2a2EtcLdgkc",
-                "wt2VfjYQyuvwftih",
-                "xkUpS2XBuSdyVEah",
-                "LV2skOm8hCwM1JRH"
-            ];
-            const tableUUID = tableUUIDs[item.system.level];
-            if (tableUUID) {
-                const table = await fromUuid(`Compendium.elkan5e.elkan5e-roll-tables.RollTable.${tableUUID}`);
-                console.log("elkan5e table", table);
-                if (table) {
-                    table.draw();
-                }
-            } else {
-                console.log("No table found for this spell level.");
-            }
-        }
-    }
+    wildSurge(activity);
 });
 
 /**
- * Handle post-use activity for Infused Healer
- *   @param {object} activity - The activity performed.
- *   @param {object} usageConfig - The usage configuration.
- *   @param {object} results - The results of the activity.
+ * Handle post-use activity for Infused Healer.
+ * @param {object} activity - The activity performed.
+ * @param {object} usageConfig - The usage configuration.
+ * @param {object} results - The results of the activity.
  */
 Hooks.on("dnd5e.postUseActivity", (activity, usageConfig, results) => {
-    let actor = activity.actor;
-    infuseHeal(actor, activity, usageConfig);
+    infuseHeal(activity, usageConfig);
 });
 
 /**
- * Handle healing overflow on damage calculation
- *   @param {object} actor - The actor being damaged.
- *   @param {object} damages - The damage descriptions.
- *   @param {object} options - Additional damage application options.
- */
-// Hooks.on("dnd5e.calculateDamage", (actor, damages, options) => {
-//     console.log("calculateDamage Hook Triggered", { actor, damages, options });
-//     // const item = options.item;
-//     // const roll = damages[0]; // Assuming the first damage roll is the one to consider
-//     // healOver(item, roll);
-// });
-
-/**
- * Handle pre-roll initiative for various features
- *   @param {object} actor - The actor rolling initiative.
- *   @param {object} roll - The resulting roll.
+ * Handle pre-roll initiative for various features.
+ * @param {object} actor - The actor rolling initiative.
+ * @param {object} roll - The resulting roll.
  */
 Hooks.on("dnd5e.preRollInitiative", (actor, roll) => {
     console.log("preRollInitiative Hook Triggered", { actor, roll });
