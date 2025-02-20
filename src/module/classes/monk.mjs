47989cfbca605177bdf275b139e0c18e8a2e5bcc
etoy22 <ethanleider@gmail.com>
2025-02-19 20:56:26 -0500

    Implemented Meld with Shadow and Empty Body
    Implemented Meld with Shadow and Empty Body

--- src/module/classes/monk.mjs
+++ src/module/classes/monk.mjs
@@ -9,9 +9,75 @@
  * @returns {Promise<void>} A promise that resolves when the effect has been processed.
  */
 export async function meldWithShadow(actor){
-    const meldEffect = actor.effects.find(i => i.name === "Meld with Shadows [Effect]");
-    if (meldEffect && actor.effects.find(i => i.name !== "Meld with Shadows [Attacks]")){
+    const meldEffect = actor.effects.find(i => i.name === game.i18n.localize("elkan5e.monk.meldWithShadowsEffect"));
+    if (meldEffect && actor.effects.find(i => i.name !== game.i18n.localize("elkan5e.monk.meldWithShadowsAttacks"))){
         await meldEffect.delete(); 
+        if (actor.effects.find(i => i.name === game.i18n.localize("elkan5e.monk.emptyBody"))){
+            await actor.effects.find(i => i.name === game.i18n.localize("elkan5e.monk.emptyBody")).delete();
+        }
     }
 }
 
+/**
+ * Handle the "Hijack Shadow" effect for the given actor.
+ * 
+ * This function checks if the actor has the "Hijack Shadow [Effect]" active.
+ * If the actor also has any other effect that is not "Hijack Shadow [Attacks]",
+ * the "Hijack Shadow [Effect]" will be deleted.
+ * 
+ * @param {object} actor - The actor object to be processed.
+ * @returns {Promise<void>} A promise that resolves when the effect has been processed.
+ */
+export async function hijackShadow(actor){
+    const hijackEffect = actor.effects.find(i => i.name === game.i18n.localize("elkan5e.monk.hijackShadowEffect"));
+    if (hijackEffect && actor.effects.find(i => i.name !== game.i18n.localize("elkan5e.monk.hijackShadowAttacks"))){
+        await hijackEffect.delete(); 
+        if (actor.effects.find(i => i.name === game.i18n.localize("elkan5e.monk.emptyBody"))){
+            await actor.effects.find(i => i.name === game.i18n.localize("elkan5e.monk.emptyBody")).delete();
+        }
+    }
+}
+
+export async function shadowMonk(activity){
+    const item = activity.item;
+    if (item.name === game.i18n.localize("elkan5e.monk.meldWithShadows") || item.name === game.i18n.localize("elkan5e.monk.hijackShadow")){
+        const actor = activity.actor;
+        if(actor.items.find(i => i.name === game.i18n.localize("elkan5e.monk.emptyBody"))){
+            const emptyBody = {
+                "_id": "4dvYtqvbQGDsVi51",
+                "changes": [
+                    { "key": "system.traits.dr.value", "mode": 0, "value": "bludgeoning", "priority": 20 },
+                    { "key": "system.traits.dr.value", "mode": 0, "value": "piercing", "priority": 20 },
+                    { "key": "system.traits.dr.value", "mode": 0, "value": "slashing", "priority": 20 },
+                    { "key": "system.traits.dv.value", "mode": 0, "value": "radiant", "priority": 20 }
+                ],
+                "disabled": false,
+                "duration": { "seconds": 600 },
+                "origin": "Item.xqRleciuHDZlYCl6",
+                "name": game.i18n.localize("elkan5e.monk.emptyBody"),
+                "statuses": ["invisible"],
+                "img": "icons/magic/perception/silhouette-stealth-shadow.webp",
+                "type": "base"
+            };
+
+            new Dialog({
+                title: game.i18n.localize("elkan5e.dialog.emptyBodyTitle"),
+                content: `<p>${game.i18n.localize("elkan5e.dialog.emptyBodyContent")}</p>`,
+                buttons: {
+                    yes: {
+                        icon: "<i class='fas fa-check'></i>",
+                        label: game.i18n.localize("elkan5e.dialog.yes"),
+                        callback: async () => {
+                            await actor.createEmbeddedDocuments("ActiveEffect", [emptyBody]);
+                        }
+                    },
+                    no: {
+                        icon: "<i class='fas fa-times'></i>",
+                        label: game.i18n.localize("elkan5e.dialog.no")
+                    }
+                },
+                default: "no"
+            }).render(true);
+        }
+    }
+}
