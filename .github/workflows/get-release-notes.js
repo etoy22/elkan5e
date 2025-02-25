f8a89f3f865e30281551d81b00bd9ca302d11214
etoy22 <ethanleider@gmail.com>
2025-02-25 11:36:27 -0500

    Updated workflow

--- .github/workflows/get-release-notes.js
+++
@@ -1,4 +0,0 @@
-let fs = require('fs');
-let changelog = fs.readFileSync('./CHANGELOG.md', 'utf8');
-let releaseNotes = changelog.split('\n').slice(2).join('\n').split(/^# v/)[1].split('\n').slice(1).join('\n');
-console.log(releaseNotes);
