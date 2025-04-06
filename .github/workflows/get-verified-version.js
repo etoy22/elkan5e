40c90ba48ebd3c865a04052fa492ae044c36e78b
etoy22 <ethanleider@gmail.com>
2025-04-06 17:39:58 -0400

    Updating code again

--- .github/workflows/get-verified-version.js
+++ .github/workflows/get-verified-version.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 
 const moduleJson = JSON.parse(fs.readFileSync('module.json', 'utf8'));
-console.log(moduleJson.systemVersion || '12.0');
+console.log(moduleJson.verifiedVersion || '12.331');
