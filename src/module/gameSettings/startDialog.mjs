aa4d97b3a476d2b0efe3cadc0d004d52a6c1778a
etoy22 <ethanleider@gmail.com>
2025-04-08 02:07:44 -0400

    Update Text

--- src/module/gameSettings/startDialog.mjs
+++ src/module/gameSettings/startDialog.mjs
@@ -8,38 +8,47 @@ export async function startDialog() {
     const SAVED_VERSION = game.settings.get("elkan5e", "moduleVersion");
     const DIALOG_SHOWN = game.settings.get("elkan5e", "dialogShown");
     const MODULE_VERSION = await getModuleVersion();
-
+    let updateNotice = false
     let saved_version = SAVED_VERSION.split('.');
     if (SAVED_VERSION !== MODULE_VERSION || !DIALOG_SHOWN) {
+        let content = `
+            <h2>${game.i18n.localize("elkan5e.dialog.content.header")}</h2>
+            <p>${game.i18n.localize("elkan5e.dialog.content.headerText")}</p>
+        `;
+            
         if (parseInt(saved_version[1]) < 12 || parseInt(saved_version[2]) < 9) {
-            //This is where we add the warning for updating to V4 of DND 5e
+            content += `
+                <h3>${game.i18n.localize("elkan5e.dialog.content.update")}</h3>
+                <p>${game.i18n.localize("elkan5e.dialog.content.updateNotice")}</p>
+            `;
         }
+
+        content += `
+            <h3>${game.i18n.localize("elkan5e.dialog.content.joinUs")}</h3>
+            <p>${game.i18n.localize("elkan5e.dialog.content.joinUsText")}</p>
+            <h3>${game.i18n.localize("elkan5e.dialog.content.supportUs")}</h3>
+            <p>${game.i18n.localize("elkan5e.dialog.content.supportText")}</p>
+            <a href='https://ko-fi.com/P5P710UQX0' target='_blank'><img height='36' style='border:0px;height:36px;' src='https://storage.ko-fi.com/cdn/kofi1.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
+        `;
+
         let dialog = new Dialog({
             title: game.i18n.localize("elkan5e.dialog.title"),
-            content: `
-                <h2>${game.i18n.localize("elkan5e.dialog.content.header")}</h2>
-                <p>${game.i18n.localize("elkan5e.dialog.content.updateNotice")}</p>
-                <h3><br>${game.i18n.localize("elkan5e.dialog.content.joinUs")}</h3>
-                <p>${game.i18n.localize("elkan5e.dialog.content.joinUsText")}</p>
-                <h3><br>${game.i18n.localize("elkan5e.dialog.content.supportUs")}</h3>
-                <p>${game.i18n.localize("elkan5e.dialog.content.supportText")}</p>
-                <a href='https://ko-fi.com/P5P710UQX0' target='_blank'><img height='36' style='border:0px;height:36px;' src='https://storage.ko-fi.com/cdn/kofi1.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
-            `,
+            content: content,
             buttons: {
-                close: {
-                    label: game.i18n.localize("elkan5e.dialog.doNotShow"),
-                    callback: async () => {
-                        await game.settings.set("elkan5e", "dialogShown", true);
-                        await game.settings.set("elkan5e", "moduleVersion", MODULE_VERSION);
-                        dialog.close();
-                    }
+            close: {
+                label: game.i18n.localize("elkan5e.dialog.doNotShow"),
+                callback: async () => {
+                await game.settings.set("elkan5e", "dialogShown", true);
+                await game.settings.set("elkan5e", "moduleVersion", MODULE_VERSION);
+                dialog.close();
                 }
+            }
             },
             close: async (html) => {
-                const buttonClicked = html.find('button').length > 0;
-                if (!buttonClicked) {
-                    await game.settings.set("elkan5e", "dialogShown", false);
-                }
+            const buttonClicked = html.find('button').length > 0;
+            if (!buttonClicked) {
+                await game.settings.set("elkan5e", "dialogShown", false);
+            }
             }
         });
         dialog.render(true);
