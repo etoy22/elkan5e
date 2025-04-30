4eaaf5211004f5787d298477b35d7af8c35ea605
etoy22 <ethanleider@gmail.com>
2025-04-29 20:08:24 -0400

    Update

---
+++ helperCode/helping/constants.py
@@ -0,0 +1,32 @@
+
+skills = ["acr", "ani", "arc", "ath", "dec", "his", "ins", "itm", "inv",
+    "med", "nat", "prc", "prf", "per", "rel", "slt", "ste", "sur"]
+skill_labels = ["acrobatics", "animal handling", "arcana", "athletics", "deception",
+    "history", "insight", "intimidation", "investigation", "medicine",
+    "nature", "perception", "performance", "persuasion", "religion",
+    "sleight of hand", "stealth", "survival"]
+conditions = [
+    "blinded", "charmed", "concentrating", "confused", "cursed", "dazed",
+    "deafened", "diseased", "drained", "exhaustion", "frightened", "goaded",
+    "grappled", "half cover", "hasted", "heavily obscured", "incapacitated",
+    "invisible", "lightly obscured", "paralyzed", "petrified", "poisoned",
+    "prone", "restrained", "silenced", "siphoned", "slowed", "stunned",
+    "surprised", "three quarters cover", "transformed", "unconscious", "weakened"
+]
+condition_labels = ["halfcover", "heavilyobscured", "lightlyobscured", "threequarterscover"]
+creature_labels = ["aberration", "beast", "celestial", "construct", "dragon", "elemental", "fey", "fiend", "giant", "humanoid", "monstrosity", "ooze", "plant", "undead"]
+damage_types = ["acid", "bludgeoning", "cold", "lightning", "fire", "force", "necrotic", "piercing", "poison", "psychic", "radiant", "slashing", "thunder"]
+schools_of_magic = ["abj", "con", "div", "enc", "evo", "ill", "nec", "trs"]
+school_labels = ["Abjuration", "Conjuration", "Divination", "Enchantment", "Evocation", "Illusion", "Necromancy", "Transmutation"]
+rules = [
+    "attack", "opportunityattacks", "dodge", "dash", "disengage", 
+    "help", "hide", "ready", "search", "surprise", 
+    "unarmedstrike", "twoweaponfighting", "spellSlots", "spellLevel",
+    "cantrips", "upcasting", "castingAtHigherLevel", "multipleSpellsInATurn",
+    "duplicateMagicalEffects", "lineOfSight", "coverAndWalls", "castingInArmor",
+    "castingTime", "spellTargets", "spellRange", "verbal", "spellDuration",
+    "illusoryImages", "knownSpells", "preparedSpells", "abilitySpells",
+    "focusSpells", "spellScroll", "cursed", "material", "ritual", "vocal", "somatic"
+]
+
+references = skills + skill_labels + conditions + condition_labels + creature_labels + damage_types + schools_of_magic + school_labels + rules
