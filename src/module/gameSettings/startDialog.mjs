6bd5ea45a17a4f9347086f9afc97ec9a9280823a
etoy22 <ethanleider@gmail.com>
2025-06-06 15:39:39 -0400

    Updated starting dialog text

--- src/module/gameSettings/startDialog.mjs
+++ src/module/gameSettings/startDialog.mjs
@@ -21,12 +21,11 @@ export async function startDialog() {
         if (now < showUntil) {
             content += `
                 <h3 style="font-weight: 800;">Elkan 5e Survey on 2024 Changes</h3>
+                <p><a href="https://forms.gle/k6yYNFR6z5djgttk7" target="_blank" title="https://forms.gle/k6yYNFR6z5djgttk7">Click here</a></p>
                 <p>The SRD (System Reference Document) is the free-to-use document of 5e rules that Elkan 5e was based on. This year, it was updated with new content.</p>
-                <p>We have been going through the new 5e 2024 SRD and picking out our favorite changes to include in the Elkan 5e system.</p>
-                <p>We've put together a form here. We'd appreciate you all weighing in and helping us decide what to do about a few possible changes to Elkan 5e:
-                <a href="https://forms.gle/k6yYNFR6z5djgttk7" target="_blank" title="https://forms.gle/k6yYNFR6z5djgttk7">
-                    click here to fill out the form
-                </a></p>
+                <p>We've been reviewing the 2024 update to the 5e SRD and selecting our favorite changes to include in the Elkan 5e system.</p>
+                <p>We'd love your input on a few possible changes we're considering. Please use the form linked above to share your thoughts.</p>
+
             `;
         }
 
@@ -41,8 +40,6 @@ export async function startDialog() {
         content += `
             <h3>${game.i18n.localize("elkan5e.dialog.content.joinUs")}</h3>
             <p>${game.i18n.localize("elkan5e.dialog.content.joinUsText")}</p>
-            <h3>${game.i18n.localize("elkan5e.dialog.content.supportUs")}</h3>
-            <p>${game.i18n.localize("elkan5e.dialog.content.supportText")}</p>
             <a href='https://ko-fi.com/P5P710UQX0' target='_blank'><img height='36' style='border:0px;height:36px;' src='https://storage.ko-fi.com/cdn/kofi1.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
         `;
         
