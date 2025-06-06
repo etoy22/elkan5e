ed2e24646864cc0f15ce25acb9745aa02cdf9a55
etoy22 <ethanleider@gmail.com>
2025-06-06 00:25:02 -0400

    Auto Replace Spells and Items work
    Still have to do Features/Class/Subclass

--- src/module/gameSettings/gameSettingRegister.mjs
+++ src/module/gameSettings/gameSettingRegister.mjs
@@ -1,5 +1,5 @@
+import {UpdateElkanRunner } from "./UpdateElkanRunner.mjs";
 export async function gameSettingRegister() {
-
     game.settings.register("elkan5e", "dialogShown", {
         name: "elkan5e.dialog.name",
         hint: "elkan5e.dialog.hint",
@@ -84,7 +84,7 @@ export async function gameSettingRegister() {
         scope: "client",
         config: false,
         type: String,
-        default: "1.12.10",
+        default: "1.13.0",
     });
 
     game.settings.register("elkan5e", "languageSystem", {
@@ -97,4 +97,15 @@ export async function gameSettingRegister() {
         type: Boolean,
         restricted: true,
     });
+
+    game.settings.registerMenu("elkan5e", "updateElkanMenu", {
+        name: "elkan5e.updateElkan.name",
+        label: "elkan5e.updateElkan.label", // Text on the button
+        hint: "elkan5e.updateElkan.hint",
+        icon: "fas fa-sync", // FontAwesome icon
+        type: UpdateElkanRunner, // Class that runs your function
+    restricted: true
+});
+
+
 }
