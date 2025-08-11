aadea7bfd65fa59361c300cc7876d148943bdba8
Ethan Leider <ethanleider@gmail.com>
2025-08-10 20:26:29 -0400

    v1.13.0

--- .github/workflows/scripts/update-module.js
+++ .github/workflows/scripts/update-module.js
@@ -11,12 +11,6 @@ function updateModuleJson(version, compatibility) {
 	moduleJson.compatibility.minimum = compatibility.minimum;
 	moduleJson.compatibility.verified = compatibility.verified;
 
-	if (compatibility.maximum !== null && compatibility.maximum !== undefined) {
-		moduleJson.compatibility.maximum = compatibility.maximum;
-	} else if ('maximum' in moduleJson.compatibility) {
-		delete moduleJson.compatibility.maximum;
-	}
-
 	fs.writeFileSync(filePath, JSON.stringify(moduleJson, null, 2), 'utf8');
 	console.log(`Updated module.json with version ${version} and compatibility.`);
 }
@@ -25,7 +19,6 @@ const version = process.argv[2];
 const compatibility = {
 	minimum: process.argv[3],
 	verified: process.argv[4],
-	maximum: process.argv[5] === 'null' ? null : process.argv[5],
 };
 
 updateModuleJson(version, compatibility);
