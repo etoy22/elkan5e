6e92259c9f09fcfd897594adaa6172cd3e0188f7
Ethan Leider <ethanleider@gmail.com>
2025-10-19 00:31:44 -0400

    Fixed Tools (before doing the New Tools)

--- src/module/rules/tools.mjs
+++ src/module/rules/tools.mjs
@@ -18,48 +18,37 @@ export function tools() {
 		"weaver",
 	];
 	const TOOLS = {
-		painter: {
-			id: "Compendium.elkan5e.elkan5e-equipment.Item.qZe6ua1j3TtX3QGv",
-		},
-		sculpt: {
-			ability: "dex",
-			id: "Compendium.elkan5e.elkan5e-equipment.Item.6x1yyzDnWGRNRwJD",
-		},
-		alchemist: {
-			id: "Compendium.elkan5e.elkan5e-equipment.Item.5TRgfcXqzHRvcYql",
-		},
-		brewer: {
-			id: "Compendium.elkan5e.elkan5e-equipment.Item.sETCxXco4DFwc95F",
-		},
-		calligrapher: {
-			id: "Compendium.elkan5e.elkan5e-equipment.Item.PSBsNmH0WWBmwTMY",
-		},
+		painter: { id: "Compendium.elkan5e.elkan5e-equipment.Item.qZe6ua1j3TtX3QGv", },
+		sculpt: { id: "Compendium.elkan5e.elkan5e-equipment.Item.6x1yyzDnWGRNRwJD", },
+		alchemist: { id: "Compendium.elkan5e.elkan5e-equipment.Item.5TRgfcXqzHRvcYql", },
+		brewer: { id: "Compendium.elkan5e.elkan5e-equipment.Item.sETCxXco4DFwc95F", },
+		calligrapher: { id: "Compendium.elkan5e.elkan5e-equipment.Item.PSBsNmH0WWBmwTMY", },
 		cook: { id: "Compendium.elkan5e.elkan5e-equipment.Item.oUY4OJSPtFBSuMJ5" },
 		herb: { id: "Compendium.elkan5e.elkan5e-equipment.Item.LQiHNDl73EtsWxj1" },
-		jeweler: {
-			id: "Compendium.elkan5e.elkan5e-equipment.Item.0xX2toS9DTCJapez",
-		},
-		leatherworker: {
-			id: "Compendium.elkan5e.elkan5e-equipment.Item.SMTANmxywrhJQVmr",
-		},
+		jeweler: { id: "Compendium.elkan5e.elkan5e-equipment.Item.0xX2toS9DTCJapez", },
+		leatherworker: { id: "Compendium.elkan5e.elkan5e-equipment.Item.SMTANmxywrhJQVmr", },
 		mason: { id: "Compendium.elkan5e.elkan5e-equipment.Item.daNXsoFk7oYAeDRt" },
 		pois: { id: "Compendium.elkan5e.elkan5e-equipment.Item.PGHsgtscRcDrKt6j" },
 		smith: { id: "Compendium.elkan5e.elkan5e-equipment.Item.aFpNp019JYV5bcgI" },
-		tailor: {
-			ability: "dex",
-			id: "Compendium.elkan5e.elkan5e-equipment.Item.C3PHE8sJKlVZs9bH",
-		},
-		tinker: {
-			id: "Compendium.elkan5e.elkan5e-equipment.Item.2Yeb51C04CmAhbsv",
-		},
-		woodcarver: {
-			id: "Compendium.elkan5e.elkan5e-equipment.Item.FDsEsGSSPWhqwjlE",
-		},
+		tailor: { id: "Compendium.elkan5e.elkan5e-equipment.Item.C3PHE8sJKlVZs9bH", },
+		tinker: { id: "Compendium.elkan5e.elkan5e-equipment.Item.2Yeb51C04CmAhbsv", },
+		woodcarver: { id: "Compendium.elkan5e.elkan5e-equipment.Item.FDsEsGSSPWhqwjlE", },
 		disg: { id: "Compendium.elkan5e.elkan5e-equipment.Item.ArsBY1Z7YMKYCWp5" },
 		navg: { id: "Compendium.elkan5e.elkan5e-equipment.Item.lExi8Y6jXHBv8vd2" },
 		thief: { id: "Compendium.elkan5e.elkan5e-equipment.Item.LMnHshpVe3ciVIwF" },
 		forg: { id: "Compendium.elkan5e.elkan5e-equipment.Item.dFa31G5pfZ3FGcjk" },
 		game: { id: "Compendium.elkan5e.elkan5e-equipment.Item.xC669rfIiGBpAALC" },
+		bagpipes: { "id": "Compendium.elkan5e.elkan5e-equipment.Item.bltntEoiFJvXR939" },
+		drum: { "id": "Compendium.elkan5e.elkan5e-equipment.Item.VLTPhQqTAOk2LPHW" },
+		dulcimer: { "id": "Compendium.elkan5e.elkan5e-equipment.Item.v72QLYutEG6w5ZRI" },
+		flute: { "id": "Compendium.elkan5e.elkan5e-equipment.Item.uDJgqlZsYrqLrNEX" },
+		horn: { "id": "Compendium.elkan5e.elkan5e-equipment.Item.PgP06DVVwXZNPMOW" },
+		lute: { "id": "Compendium.elkan5e.elkan5e-equipment.Item.yQy1cBGK6P3eD4GZ" },
+		lyre: { "id": "Compendium.elkan5e.elkan5e-equipment.Item.BrCG6C5mCLPahCml" },
+		panflute: { "id": "Compendium.elkan5e.elkan5e-equipment.Item.VC36HOskyNfD6DXV" },
+		shawm: { "id": "Compendium.elkan5e.elkan5e-equipment.Item.ZN6lVBJAeNqt4EE9" },
+		viol: { "id": "Compendium.elkan5e.elkan5e-equipment.Item.rrNz3sZQQdb8ygRW" }
+
 	};
 
 	// Adding Tool Types
